9597b932052cd11581219fa235271f6d859d02cf
Owen <ocrewe04@gmail.com>
2021-12-12 13:38:57 -0800

    init commit

---
+++ pages/hiking.js
@@ -0,0 +1,14 @@
+import { Container, Heading } from "@chakra-ui/react";
+import Section from "../components/section";
+
+const Hiking = () => {
+  return (
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Hiking
+      </Heading>
+    </Container>
+  )
+}
+
+export default Hiking
---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import NextLink from 'next/link'
+import { 
+  Container,
+  Box,
+  Heading,
+  Image,
+  Link
+} from '@chakra-ui/react'
+import Section from '../components/section'
+import Paragraph from '../components/paragraph'
+
+const Page = () => {
+  return (
+    <Container>
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+          <Heading as="h2" variant="page-title">
+            Owen Crewe
+          </Heading>
+          <p>Software Engineering Student @ UVic</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Image
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            maxWidth="100px"
+            display="inline-block"
+            borderRadius="full"
+            src="/images/owen.jpg"
+            alt="Profile image"
+          />
+        </Box>
+      </Box>
+
+      <Section delay={0.1}>
+        <Heading as='h3' variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Body... link to my{' '}
+          <NextLink href="/resume.pdf" passHref>
+            <Link target="_blank" rel="noopener noreferrer">resume</Link>
+          </NextLink>
+          .
+        </Paragraph>
+      </Section>
+
+      <Section delay={0.2}>
+        <Heading as='h3' variant="section-title">
+          Hobbies
+        </Heading>
+        <Paragraph>
+          Climbing,{' '}
+          <NextLink href="/hiking">
+            <Link>hiking</Link>
+          </NextLink>
+        </Paragraph>
+      </Section>
+    </Container>
+  )
+}
+
+export default Page
